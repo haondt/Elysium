c9bbb9919839baf6855fe6411c8cdb8da9bc6b4f
Noah <burghardtnoah@gmail.com>
2024-09-14 11:32:42 -0600

    finished home page prototype

--- prototypes/serve.py
+++ prototypes/serve.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, abort, send_from_directory
 import os
+from faker import Faker
+from datetime import datetime,timedelta
+import random
 
 app = Flask(__name__)
+fake = Faker()
 
 # Base directory where your templates are stored
 TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
@@ -9,6 +13,19 @@ app.template_folder = TEMPLATE_DIR
 
 # Directory where assets are stored
 ASSET_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets')
+
+def fake_ts():
+    r = random.random()
+    tmax, ttype = random.choice([
+        (None, 'just now'),
+        (60, 's'),
+        (60, 'm'),
+        (12, 'h')
+    ])
+    if tmax is not None:
+        return f'{random.randint(1, tmax)}{ttype}'
+    return ttype
+
 
 @app.route('/<path:template_path>')
 def dynamic_render(template_path):
@@ -23,7 +40,7 @@ def dynamic_render(template_path):
     template_vars = {key: value for key, value in request.args.items()}
 
     # Render the template and pass the dictionary
-    return render_template(template_file, params=template_vars)
+    return render_template(template_file, params=template_vars, faker=fake, fake_ts=fake_ts, random=random)
 
 # Serve assets from the /_asset/ route
 @app.route('/_asset/<path:filename>')
